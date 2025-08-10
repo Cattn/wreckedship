451880d3df51c9528fb985938b24b7c560ed9cc8
Logan <thecrazykid2121@gmail.com>
2025-08-10 03:18:50 -0400

    add back

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ app.use(express.static(path.join(__dirname, 'client')));
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy',
+      "default-src 'self' https: http: data: blob:; script-src 'self' https://cdn.jsdelivr.net 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval'; connect-src 'self' https: http: ws: wss:; img-src 'self' data: blob:; style-src 'self' 'unsafe-inline'; worker-src 'self' blob:");
+    next();
+  });
