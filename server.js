ddf32739e52fcde47f1bcc79c173e3fcdb0f4b12
Logan <thecrazykid2121@gmail.com>
2025-08-10 03:44:15 -0400

    serve statically because maybe

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = 4032;
 
+const socketIoClientPath = path.join(__dirname, 'api', 'node_modules', 'socket.io-client', 'dist', 'socket.io.min.js');
+
+app.get('/vendor/socket.io.min.js', (req, res) => {
+  res.type('application/javascript');
+  res.sendFile(socketIoClientPath);
+});
+
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.listen(PORT, () => {
